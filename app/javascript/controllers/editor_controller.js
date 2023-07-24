0b91baae24c8f67671103eee72c7001fa06fb686
drotaenko <drotaenko@xbsoftware.by>
2023-07-25 00:38:39 +0300

    remove unused code

--- app/javascript/controllers/editor_controller.js
+++ app/javascript/controllers/editor_controller.js
@@ -7,24 +7,6 @@ export default class extends Controller {
     tinymce.init({
       selector: ".tinymce",
       promotion: false,
-      toolbar: 'fullscreen',
-      plugins: 'fullscreen'
     });
-  }
-
-  saveEditorData(event) {
-    // event.preventDefault();
-    // const pageForm = this.element
-    // const editorContent = this.contentEditor.getHTML()
-    // const dataToSave = editorContent === "" ? "<p></p>" : editorContent;
-    // const hiddenContentField = this.page_content_hiddenTarget;
-    //
-    // hiddenContentField.value = dataToSave;
-    //
-    // pageForm.submit();
-  }
-
-  getPageCurrentContent() {
-    // return this.page_content_hiddenTarget.value;
   }
 }
